ed1a0d20c6eeb0fc8f252b856d4f4469460d6d6d
Nicole Laczny <nicolelaczny@gmail.com>
2019-11-14 18:23:14 -0600

    real like fruits

---
+++ Garden_Project/Assets/Nicoles materials/treeFruit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class treeFruit : MonoBehaviour
+{
+    public GameObject physicsOn;
+    public GameObject physicsOff;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void swapTheFruit()
+    {
+
+        if (!physicsOn.activeInHierarchy)
+        {
+            physicsOn.SetActive(true);
+            physicsOff.SetActive(false);
+        }
+    }
+}
