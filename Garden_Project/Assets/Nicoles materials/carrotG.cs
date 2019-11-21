f0c4a2ad90f35c766978d5700cc0d8b8ea533390
Nicole Laczny <nicolelaczny@gmail.com>
2019-11-21 17:57:49 -0600

    carrot + veggie

---
+++ Garden_Project/Assets/Nicoles materials/carrotG.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carrotG : MonoBehaviour
+{
+    public GameObject physicsOn;
+    public Collider m_Collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void turnonG()
+    {
+        physicsOn.GetComponent<Rigidbody>().useGravity = true;
+        m_Collider = GetComponent<Collider>();
+        m_Collider.enabled = enabled;
+    }
+}
