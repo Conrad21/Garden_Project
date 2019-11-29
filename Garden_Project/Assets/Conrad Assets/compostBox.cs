7effa01e033a20efaae851b09b3a64b8596e1a2a
siggamesuic@gmail.com <Backward2$>
2019-11-29 14:42:51 -0600

    added compost pile

--- Garden_Project/Assets/Conrad Assets/compostBox.cs
+++ Garden_Project/Assets/Conrad Assets/compostBox.cs
@@ -4,28 +4,40 @@ using UnityEngine;
 
 public class compostBox : MonoBehaviour
 {
+    public int clockMultiplier = 1;
     public GameObject compost1;
     public GameObject compost2;
     public GameObject compost3;
+    public GameObject Readycompost1;
+    public GameObject Readycompost2;
+    public GameObject Readycompost3;
     public GameObject compostsmall;
     public GameObject compostmed;
     public GameObject compostbig;
-  //  public GameObject compostsound;
-    private int counter = 1;
+    //  public GameObject compostsound;
+    public int counter = 0;
     public AudioClip AudioClip;
     public AudioSource AudioSource;
 
+    private float timer = 0.0f;
+    public float waitTime = 10.0f;
+
+    private int Compostcounter = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        waitTime = waitTime * clockMultiplier;
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        updateCompost(); 
     }
 
     void OnCollisionEnter(Collision c)
@@ -34,36 +46,84 @@ public class compostBox : MonoBehaviour
         {
             AudioSource.Play();
 
-
+            counter = counter + 1;
+            timer = 0.0f;
             compost1.SetActive(false);
             compost2.SetActive(false);
             compost3.SetActive(false);
-            if (counter == 1)
-            {
-                compostsmall.SetActive(true);
-                counter = 2; 
-            }
 
-            else if (counter == 2)
-            {
-                compostsmall.SetActive(false);
-                compostmed.SetActive(true);
-                counter = 3; 
-            }
-
-            else if (counter == 3)
-            {
-                compostmed.SetActive(false);
-                compostbig.SetActive(true);
-            }
+          
 
         }
 
 
-
-
     }
 
+    void updateCompost()
+    {
 
+        if (counter == 0)
+        {
+            Debug.Log("counter++");
+            compostsmall.SetActive(false);
+
+        }
+
+
+        if (counter > 0)
+        {
+
+            //Debug.Log("hehehe");
+            compostsmall.SetActive(true);
+
+        }
+
+        else if (counter >= 3)
+        {
+            compostsmall.SetActive(false);
+            compostmed.SetActive(true);
+
+        }
+
+        else if (counter >= 6)
+        {
+            compostmed.SetActive(false);
+            compostbig.SetActive(true);
+
+        }
+
+
+        if (counter >= 1 && counter < 3 && (timer > waitTime))
+        {
+            Debug.Log("hhehehe");
+            counter = counter - 1;
+            Readycompost1.SetActive(true);
+     
+            timer = 0.0f;
+        }
+
+        if (counter >= 3 && counter < 6 && (timer > waitTime))
+        {
+
+            counter = counter - 1;
+            Readycompost1.SetActive(false);
+            Readycompost2.SetActive(true);
+ 
+            timer = 0.0f;
+
+        }
+
+        if (counter >= 6 && (timer > waitTime))
+        {
+
+            counter = counter - 1;
+            Readycompost1.SetActive(false);
+            Readycompost2.SetActive(false);
+            Readycompost1.SetActive(true);
+            timer = 0.0f;
+
+
+        }
+    }
 
 }
