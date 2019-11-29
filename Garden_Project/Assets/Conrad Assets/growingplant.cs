d58de0fb3f957e2fea983622515d0d4d21214c6c
siggamesuic@gmail.com <Backward2$>
2019-11-29 00:34:33 -0600

    yeeeeeee

--- Garden_Project/Assets/Conrad Assets/growingplant.cs
+++ Garden_Project/Assets/Conrad Assets/growingplant.cs
@@ -6,40 +6,52 @@ public class growingplant : MonoBehaviour
 {
   
 
-    public int m = 1;
+    public int clockMultiplier = 1;
+    public float compostSpeedUp = .75f;
+
+    public GameObject compflag; 
     public GameObject cabbage;
     public GameObject littlecab;
     public GameObject seedling;
     public GameObject hole;
-    public GameObject deadplant; 
-    //public GameObject lettuse;
-    //public GameObject flower1;
-    /// public GameObject pumpkin;
-  //  public GameObject ground;
+    public GameObject deadplant;
+    public GameObject tallblueflower;
+    public GameObject smalltallblueflower;
+    public GameObject mushroom;
+    public GameObject smallmushroom;
+    public GameObject caladium;
+    public GameObject smallCaladium;
+    public GameObject sunflower;
+    public GameObject smallSunflower;
+    public GameObject purpleFlower;
+    public GameObject smallpurpleflowers;
+    public GameObject pumpkin;
+    public GameObject smallPumpkin;
+    public GameObject Bocchoy;
+    public GameObject smallBocchoy;
+    public GameObject readyCompost;
+    public GameObject smallCarrot;
+    public GameObject Carrot;
+
     public float waitTime = 3.0f;
     public float waitTime2 = 10.0f;
     public float waitTime3 = 15.0f;
     public float waitwater = 120.0f;
     private float timer = 0.0f;
     private float timer2 = 0.0f;
-    public float x = 1;
-    public float y = 1;
-    public float z = 1;
+
     private bool plantIsHere = false;
     private bool isWatered = false;
     private bool plantIsAlive = false;
-    
-
+    private bool holeIsHere = false;
+    private bool waterflag = false;
 
     private bool phase1 = false;
     private bool phase2 = false;
     private bool phase3 = false;
     private bool phase4 = false;
-    private bool holeIsHere = false; 
-    private bool waterflag = false;
-    private int flag = 0;
-  
 
+    private int flag = 0;
 
     GameObject general;
     GameObject general2;
@@ -48,8 +60,8 @@ public class growingplant : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        waitTime = waitTime * m;
-        waitTime2 = waitTime2 * m;
+        waitTime = waitTime * clockMultiplier;
+        waitTime2 = waitTime2 * clockMultiplier;
     }
 
     // Update is called once per frame
@@ -111,6 +123,7 @@ public class growingplant : MonoBehaviour
             general.SetActive(false);
             general3.SetActive(false);
             deadplant.SetActive(true);
+            compflag.SetActive(false);
 
 
 
@@ -128,6 +141,8 @@ public class growingplant : MonoBehaviour
 
     void OnCollisionEnter(Collision ground)
     {
+        Debug.Log(ground.gameObject.tag);
+
         //Check for a match with the specified name on any GameObject that collides with your GameObject
         if (ground.gameObject.name == "dirt")
         {
@@ -136,6 +151,8 @@ public class growingplant : MonoBehaviour
 
         }
 
+//------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------
 
         //Check for a match with the specific tag on any GameObject that collides with your GameObject
         if (ground.gameObject.tag == "seed_cabbage" && plantIsHere == false && holeIsHere == true)
@@ -146,9 +163,7 @@ public class growingplant : MonoBehaviour
             general2 = littlecab;
             general3 = cabbage;
 
-            //Destroy(GameObject.FindWithTag("box"));
-            GameObject tmp = GameObject.FindWithTag("seed");
-            Destroy(tmp);
+            Destroy(ground.gameObject);
             hole.SetActive(false);
             plantIsHere = true;
             plantIsAlive = true;
@@ -159,6 +174,159 @@ public class growingplant : MonoBehaviour
         }
 
 
+       else if (ground.gameObject.tag == "seed_blueFlower" && plantIsHere == false && holeIsHere == true)
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            Debug.Log("Blue Seed as been planted");
+            general = seedling;
+            general2 = smalltallblueflower;
+            general3 = tallblueflower;
+
+            Destroy(ground.gameObject);
+            hole.SetActive(false);
+            plantIsHere = true;
+            plantIsAlive = true;
+
+            timer = 0.0f;
+            timer2 = 0.0f;
+            flag = 1;
+        }
+
+       else if (ground.gameObject.tag == "seed_Carrot" && plantIsHere == false && holeIsHere == true)
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            Debug.Log("Seed as been planted");
+            general = seedling;
+            general2 = smallCarrot;
+            general3 = Carrot;
+
+            Destroy(ground.gameObject);
+            hole.SetActive(false);
+            plantIsHere = true;
+            plantIsAlive = true;
+
+            timer = 0.0f;
+            timer2 = 0.0f;
+            flag = 1;
+        }
+
+
+       else if (ground.gameObject.tag == "seed_pumpkin" && plantIsHere == false && holeIsHere == true)
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            Debug.Log("Seed as been planted");
+            general = seedling;
+            general2 = smallPumpkin ;
+            general3 = pumpkin;
+
+            Destroy(ground.gameObject);
+            hole.SetActive(false);
+            plantIsHere = true;
+            plantIsAlive = true;
+
+            timer = 0.0f;
+            timer2 = 0.0f;
+            flag = 1;
+        }
+
+       else if (ground.gameObject.tag == "seed_sunflower " && plantIsHere == false && holeIsHere == true)
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            Debug.Log("Seed as been planted");
+            general = seedling;
+            general2 = smallSunflower;
+            general3 = sunflower;
+
+            Destroy(ground.gameObject);
+            hole.SetActive(false);
+            plantIsHere = true;
+            plantIsAlive = true;
+
+            timer = 0.0f;
+            timer2 = 0.0f;
+            flag = 1;
+        }
+
+       else if (ground.gameObject.tag == "seed_purpleFlower" && plantIsHere == false && holeIsHere == true)
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            Debug.Log("Seed as been planted");
+            general = seedling;
+            general2 = smallpurpleflowers;
+            general3 = purpleFlower;
+
+            Destroy(ground.gameObject);
+            hole.SetActive(false);
+            plantIsHere = true;
+            plantIsAlive = true;
+
+            timer = 0.0f;
+            timer2 = 0.0f;
+            flag = 1;
+        }
+
+        else if (ground.gameObject.tag == "seed_caladium" && plantIsHere == false && holeIsHere == true)
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            Debug.Log("Seed as been planted");
+            general = seedling;
+            general2 = smallCaladium;
+            general3 = caladium;
+
+            Destroy(ground.gameObject);
+            hole.SetActive(false);
+            plantIsHere = true;
+            plantIsAlive = true;
+
+            timer = 0.0f;
+            timer2 = 0.0f;
+            flag = 1;
+        }
+
+        else if (ground.gameObject.tag == "seed_whiteMushroom" && plantIsHere == false && holeIsHere == true)
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            Debug.Log("Seed as been planted");
+            general = seedling;
+            general2 = smallmushroom;
+            general3 = mushroom;
+
+            Destroy(ground.gameObject);
+            hole.SetActive(false);
+            plantIsHere = true;
+            plantIsAlive = true;
+
+            timer = 0.0f;
+            timer2 = 0.0f;
+            flag = 1;
+        }
+
+       else if (ground.gameObject.tag == "seed_bokchoy" && plantIsHere == false && holeIsHere == true)
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            Debug.Log("Seed as been planted");
+            general = seedling;
+            general2 = smallBocchoy;
+            general3 = Bocchoy;
+
+            Destroy(ground.gameObject);
+            hole.SetActive(false);
+            plantIsHere = true;
+            plantIsAlive = true;
+
+            timer = 0.0f;
+            timer2 = 0.0f;
+            flag = 1;
+        }
+
+
+        Debug.Log("Something touch the ground");
+
+
+
+
+
+        //====================================================================================================
 
         if (ground.gameObject.tag == "water")
         {
@@ -177,7 +345,16 @@ public class growingplant : MonoBehaviour
                 holeIsHere = true; 
                 hole.SetActive(true);
             }
+
+            if (readyCompost.activeInHierarchy)
+            {
+                waitTime = waitTime * compostSpeedUp;
+                compflag.SetActive(true);
+              
+            }
         }
+
+
 
        // Debug.Log("someting growing did touch it");
     }
