d34aa2e0b23d596530a6ea928de4930b199395ef
Nicole Laczny <nicolelaczny@gmail.com>
2019-11-30 12:18:21 -0600

    moving bugs can be killed

---
+++ Garden_Project/Assets/killMovingBug.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killMovingBug : MonoBehaviour
+{
+    public GameObject splat;
+    public GameObject bug;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+
+        //Check for a match with the specific tag on any GameObject that collides with your GameObject
+        if (collision.gameObject.tag == "swatter")
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            Debug.Log("Do something else here");
+
+            var Vector3 = bug.transform.position;
+            Instantiate(splat, Vector3, Quaternion.identity);
+            bug.SetActive(false);
+        }
+    }
+}
---
+++ Garden_Project/Assets/spawnBug.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnBug : MonoBehaviour
+{
+
+    public GameObject splat;
+    public GameObject bug;
+    public float waitTime = 20.0f;
+    private float timer = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > waitTime * 3)
+        {
+
+            var Vector3 = splat.transform.position;
+            Instantiate(bug, Vector3, Quaternion.identity);
+            splat.SetActive(false);
+
+        }
+    }
+}
