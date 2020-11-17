c47d52609017a01d420d9da98c46aecbe3739f52
Conrad21 <ConradPtasznik@gmail.com>
2020-11-17 17:43:37 -0600

    addded a pause menu

---
+++ Garden_Project/Assets/Conrad Assets/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] private bool isPaused;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+
+            if (isPaused)
+            {
+                ActivateMenu();
+            }
+            else
+            {
+                DeactivateMenu();
+            }
+        }
+    }
+
+
+    void ActivateMenu()
+    {
+        Time.timeScale = 0;
+        AudioListener.pause = true; 
+        pauseMenuUI.SetActive(true);
+    }
+
+    void DeactivateMenu()
+    {
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        pauseMenuUI.SetActive(false);
+        isPaused = false; 
+    }
+
+}
+
+
