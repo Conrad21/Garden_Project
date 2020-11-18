edd0b3059c36bb4e093962cd9d9bb2a05dcbd045
Conrad21 <ConradPtasznik@gmail.com>
2020-11-17 18:37:12 -0600

    addded simeple start menu

---
+++ Garden_Project/Assets/Conrad Assets/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    private string NewScene = "Garden scene";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(NewScene); 
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); 
+    }
+}
